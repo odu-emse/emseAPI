90828c4285c41016c16efb5868eeb25d95363539
Daniel Papp <dpapp001@odu.edu>
2020-10-31 18:39:49 -0400

    feature(search): combined search functionality
    search endpoint now returns both courses and modules as one object and users can search based on keywords, and instructor names

--- api/routes/search.js
+++ api/routes/search.js
@@ -2,30 +2,108 @@ import express from "express";
 const search = express.Router();
 import Module from "../models/Module";
 import Course from "../models/Course";
+import stringSimilarity from "string-similarity";
 
-search.post("/", (req, res, next) => {
+search.post("/", async (req, res, next) => {
 	const query = req.body.query;
+	const { selection } = req.query;
 
-	Course.find({ courseNumber: query })
-		.then(data => {
-			if (!data) {
-				return res.status(400).json({
-					conf: "failed",
-				});
-			} else if (data.length === 0) {
-				return res.status(200).json({
-					error: "No results found",
-					data: [],
-				});
-			} else {
-				res.json({
-					data,
-				});
-			}
-		})
-		.catch(err => {
-			console.log(err);
+	let courses;
+	let modules;
+
+	//creating a regex to check if string contains any numbers
+	let numberCheck = new RegExp(/^\d+$/);
+
+	//checks if query sting includes a number or not
+	if (numberCheck.test(query)) {
+		//find all the course documents and module documents
+		const rawCourses = await Course.find();
+		const rawModules = await Module.find();
+
+		//init empty array to hold all of our stringified course numbers and module numbers
+		let stringCourseNumbers = [];
+		let stringModuleNumbers = [];
+
+		rawCourses.map(course => {
+			//stringify course numbers
+			let stringNum = course.courseNumber.toString();
+			//add string course numbers into array
+			stringCourseNumbers.push(stringNum);
 		});
+		//console.log(stringCourseNumbers);
+
+		rawModules.map(module => {
+			//stringify course numbers
+			let stringNum = module.moduleNumber.toString();
+			//add string course numbers into array
+			stringModuleNumbers.push(stringNum);
+		});
+
+		let combinedArray = stringCourseNumbers.concat(stringModuleNumbers);
+
+		const compare = stringSimilarity.findBestMatch(
+			query.toString(),
+			combinedArray
+		);
+
+		console.log({ query, compare });
+
+		courses = await Course.find({ courseNumber: compare.bestMatch.target });
+		modules = await Module.find({ moduleNumber: compare.bestMatch.target });
+	}
+	//run query for courses and modules based on the string that the user passed in
+	else {
+		if (selection === "instructor") {
+			modules = await Module.find({
+				instructor: {
+					$regex: new RegExp(query, "i"),
+				},
+			});
+		} else if (selection === "key") {
+			courses = await Course.find({
+				keywords: {
+					$regex: new RegExp(query, "i"),
+				},
+			});
+			modules = await Module.find({
+				keywords: {
+					$regex: new RegExp(query, "i"),
+				},
+			});
+		} else {
+			courses = await Course.find({
+				courseName: {
+					$regex: new RegExp(query, "i"),
+				},
+			});
+			modules = await Module.find({
+				moduleName: {
+					$regex: new RegExp(query, "i"),
+				},
+			});
+		}
+	}
+
+	//check if either courses or modules returned any values from db
+	if (courses || modules) {
+		let data;
+		//see if both courses and modules returned values from db
+		if (modules && courses) {
+			let combined = modules.concat(courses);
+			data = combined;
+		} else if (courses) {
+			data = courses;
+		} else if (modules) {
+			data = modules;
+		}
+		return res.json({ data });
+	} else {
+		//adding empty array to prevent front end from breaking due to array mapping
+		return res.status(400).json({
+			error: "No results found with the provided parameters",
+			data: [],
+		});
+	}
 });
 
 export default search;
