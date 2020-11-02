93b0b15dd6e8640f33970727b8e261efc59d812f
Daniel Papp <dpapp001@odu.edu>
2020-11-02 18:13:02 -0500

    feature(courses): courses store containing modules

--- api/models/Course.js
+++ api/models/Course.js
@@ -1,37 +1,27 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const Course = new mongoose.Schema({
-    courseName: {
-        type: String,
-        trim: true,
-        default: '',
-    },
-    courseNumber: {
-        type: Number,
-        default: 0,
-    },
-    description: {
-        type: String,
-        default: ''
-    },
-    numberOfModules: {
-        type: Number,
-        default: 0
-    },
-    difficulty: {
-        type: Number,
-        default: 0
-    },
-    completed: {
-        type: Boolean
-    },
-    containingModules: [
-        //contains the _id for each module that's covered by this course
-        String
-    ],
-    keywords: [
-        String
-    ]
-})
+	courseName: {
+		type: String,
+		trim: true,
+		default: "",
+	},
+	courseNumber: {
+		type: Number,
+		default: 0,
+	},
+	description: {
+		type: String,
+		default: "",
+	},
+	difficulty: {
+		type: Number,
+		default: 0,
+	},
+	containingModules: [
+		//contains the _id for each module that's covered by this course
+	],
+	keywords: [String],
+});
 
-export default mongoose.model('Course', Course)
+export default mongoose.model("Course", Course);
--- api/routes/course.js
+++ api/routes/course.js
@@ -3,61 +3,58 @@ const course = express.Router();
 import Course from "../models/Course";
 
 course.get("/", (req, res, next) => {
-  Course.find()
-    .then(data => {
-      if (!data) {
-        return res.status(404).end;
-      } else {
-        res.status(200).json({
-          conf: "success",
-          data: data
-        });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-course.get("/:courseNumber", (req, res, next) => {
-  if (req.params.courseNumber.length > 3) {
-    next();
-  }
-  const courseNum = req.params.courseNumber;
-  Course.find({
-     courseNumber: courseNum
-  })
-    .then(data => {
-      if (!data) {
-        return res.status(404).end;
-      } else {
-        res.status(200).json({
-          conf: "success",
-          data: data
-        });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
+	Course.find()
+		.then(data => {
+			if (!data) {
+				return res.status(404).end;
+			} else {
+				res.status(200).json({
+					conf: "success",
+					data: data,
+				});
+			}
+		})
+		.catch(err => {
+			console.log(err);
+		});
 });
 
 course.get("/:id", (req, res, next) => {
-  const identifier = req.params.id;
-  Course.findById(identifier)
-    .then(data => {
-      if (!data) {
-        return res.status(404).end;
-      } else {
-        res.status(200).json({
-          conf: "success",
-          data: data
-        });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
+	const { id } = req.params;
+	Course.findById(id)
+		.then(data => {
+			if (!data) {
+				return res.status(404).end;
+			} else {
+				res.status(200).json({
+					conf: "success",
+					data: data,
+				});
+			}
+		})
+		.catch(err => {
+			console.log(err);
+		});
+});
+
+course.put("/:id", async (req, res, next) => {
+	const { id } = req.params;
+	const { module } = req.query;
+	const course = await Course.findById(id);
+	course.updateOne(
+		{
+			$push: {
+				containingModules: module,
+			},
+		},
+		(err, data) => {
+			if (err) {
+				res.status(400).json({ error: err });
+			} else {
+				res.status(200).end();
+			}
+		}
+	);
 });
 
 export default course;
