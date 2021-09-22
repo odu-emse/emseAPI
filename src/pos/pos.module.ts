938aa294cd18e8306dcb725c9cb54607a196e7c5
Daniel Papp <dpapp001@odu.edu>
2021-09-22 11:11:09 -0400

    🐛 (planofstudy): Added prisma as pos dependency
    Added PrismaService to Plan Of Study as a dependency to fix null return query issue.

--- src/pos/pos.module.ts
+++ src/pos/pos.module.ts
@@ -1,8 +1,9 @@
+import { PrismaService } from "./../prisma.service";
 import { PlanOfStudyResolver } from "./pos.resolver";
 import { Module } from "@nestjs/common";
 import { PoSService } from "./pos.service";
 
 @Module({
-	providers: [PoSService, PlanOfStudyResolver],
+	providers: [PoSService, PlanOfStudyResolver, PrismaService],
 })
 export class PoSModule {}
