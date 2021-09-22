635d22952f938c9ca548c676b60a0cb7a071b705
frontoge <coltsman1011@gmail.com>
2021-09-22 14:36:08 -0400

    Remove duplicate PrismaService import

--- src/pos/pos.module.ts
+++ src/pos/pos.module.ts
@@ -1,8 +1,7 @@
-import { PrismaService } from "./../prisma.service";
+import { PrismaService } from "../prisma.service";
 import { PlanOfStudyResolver } from "./pos.resolver";
 import { Module } from "@nestjs/common";
 import { PoSService } from "./pos.service";
-import { PrismaService } from "../prisma.service";
 
 @Module({
 	providers: [PoSService, PlanOfStudyResolver, PrismaService],
