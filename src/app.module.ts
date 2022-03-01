e4e86126dd064fc59a3d847999a473f8203f1202
Daniel Papp <dpapp001@odu.edu>
2022-03-01 11:44:24 -0500

    ➕ (deps): Added Apollo driver to NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { UserModule } from "./user/user.module";
 import { PoSModule } from "./pos/pos.module";
 import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { UserService } from "./user/user.service";
 import { UserResolver } from "./user/user.resolver";
 import { PoSService } from "./pos/pos.service";
@@ -13,10 +14,11 @@ require("dotenv").config();
 
 @Module({
 	imports: [
-		GraphQLModule.forRoot({
+		GraphQLModule.forRoot<ApolloDriverConfig>({
 			debug: true,
 			//playground: true,
 			typePaths: ["./**/*.graphql"],
+			driver: ApolloDriver,
 		}),
 		MongooseModule.forRoot(process.env.DATABASE_URL || "", {
 			useNewUrlParser: true,
