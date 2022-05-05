22fb54ee006203c126b498775d06c14db39ba455
Daniel Papp <dpapp001@odu.edu>
2022-05-05 11:34:51 -0400

    Added apollo driver for @nest/gql v10

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,16 @@ import { UserModule } from "./user/user.module";
 import { PoSModule } from "./pos/pos.module";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { UserService } from "./user/user.service";
-import { UserResolver } from "./user/user.resolver";
-import { PoSService } from "./pos/pos.service";
-import { PlanOfStudyResolver } from "./pos/pos.resolver";
-import { ProgramService } from "./program/program.service";
 import { ProgramModule } from "./program/program.module";
 require("dotenv").config();
 
 @Module({
 	imports: [
-		GraphQLModule.forRoot({
+		GraphQLModule.forRoot<ApolloDriverConfig>({
 			playground: true,
-			debug: true,
+			debug: process.env.NODE_ENV !== "production",
 			typePaths: ["./**/*.graphql"],
-			// driver: ApolloDriver,
+			driver: ApolloDriver,
 		}),
 		MongooseModule.forRoot(process.env.DATABASE_URL || "", {
 			useNewUrlParser: true,
