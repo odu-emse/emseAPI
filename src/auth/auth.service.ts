22ce028e8563db9139eaa097c63eded42c1450c0
Joel DeSante <joeldesante@gmail.com>
2022-09-29 15:00:40 -0400

    Add endpoint for refreshing the token.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,7 +13,23 @@ export class AuthService {
                 client_secret: process.env.GOOGLE_SECRET,
                 grant_type: 'authorization_code',
                 redirect_uri: 'http://localhost:3000/api/auth/redirect',
+                access_type: 'offline',
                 code
+            })
+        });
+
+        return response;
+    }
+
+    async refreshToken(token: String) {
+        const response = await fetch("https://oauth2.googleapis.com/token", {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify({
+                client_id: process.env.GOOGLE_ID,
+                client_secret: process.env.GOOGLE_SECRET,
+                grant_type: 'refresh_token',
+                refresh_token: token
             })
         });
 
