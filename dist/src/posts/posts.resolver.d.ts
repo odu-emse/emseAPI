14b06fed1db9aa358ef6a9a2b5018a0f9649e367
Dániel B. Papp <dpapp@odu.edu>
2022-11-17 15:06:10 -0500

    :wrench: (system): Ignored and deleted the dist folder

--- dist/src/posts/posts.resolver.d.ts
+++
@@ -1,6 +0,0 @@
-import { PostService } from "./posts.service";
-export declare class PostResolvers {
-    private readonly postService;
-    constructor(postService: PostService);
-    sayHello(): string;
-}
