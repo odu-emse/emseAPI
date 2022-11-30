871bf6762defd9372b243acf94d6509957069894
Dániel B. Papp <dpapp@odu.edu>
2022-11-30 11:51:22 -0500

    :sparkles: (Thread): Worked on services populating relations

--- src/community/community.service.ts
+++ src/community/community.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@nestjs/common';
 import {PrismaService} from "../prisma.service";
+import {Prisma} from "@prisma/client";
 
 @Injectable()
 export class CommunityService {
@@ -7,14 +8,38 @@ export class CommunityService {
     }
 
     async threads() {
-        return await this.prisma.thread.findMany({});
+        const include = Prisma.validator<Prisma.ThreadInclude>()({
+            comments: {
+                include: {
+                    author: true
+                }
+            },
+            parentLesson: true,
+            usersWatching: true,
+            author: true
+        })
+
+        return await this.prisma.thread.findMany({
+            include: {
+                comments: true,
+                parentLesson: true,
+                usersWatching: true,
+                author: true
+            }
+        })
     }
 
     async thread(id: string) {
-        return await this.prisma.thread.findUnique({
+        const include = Prisma.validator<Prisma.ThreadInclude>()({
+            comments: true,
+            parentLesson: true
+        })
+
+        return await this.prisma.thread.findUniqueOrThrow({
             where: {
                 id
-            }
+            },
+            include
         })
     }
 }
