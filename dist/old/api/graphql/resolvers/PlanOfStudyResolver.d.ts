b54be15b85340781385f0767d9fe2fd007b62f9c
Dániel B. Papp <dpapp@odu.edu>
2022-11-30 11:52:27 -0500

    :broom: (general): Added dist/ to git ignore

--- dist/old/api/graphql/resolvers/PlanOfStudyResolver.d.ts
+++
@@ -1,5 +0,0 @@
-import { PlanOfStudy } from "../../models/PlanOfStudy";
-export declare class PlanOfStudyResolver {
-    getPlan(id: string): Promise<import("@typegoose/typegoose").DocumentType<PlanOfStudy>>;
-    getPlans(): Promise<PlanOfStudy[]>;
-}
--- dist/old/api/graphql/types.d.ts
+++
@@ -1,2 +0,0 @@
-import { ObjectId } from "mongodb";
-export declare type Ref<T> = T | ObjectId;
