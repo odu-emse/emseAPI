afbd1b4867fb123eff2b4804ee81439bb81118b4
Dániel B. Papp <dpapp@odu.edu>
2023-01-09 13:34:37 -0500

    :test_tube: (tool): Changed test framework to vitest Got tired of waiting for jest to finish compiling all the time, so i switch it out with `vitest`. Coverage reports generated by vitest are different so our coverage score dropped significantly.

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,16 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
 
 export default {
-	"transform": {
+	transform: {
 		"^.+\\.ts$": "ts-jest"
 	},
-	rootDir: './',
-	"moduleFileExtensions": [
-		"ts",
-		"js"
-	],
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-	"testRegex": ".*\\.spec\\.ts$",
-	"testEnvironment": "node"
+	rootDir: "./",
+	moduleFileExtensions: ["ts", "js"],
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+		prefix: "<rootDir>/"
+	}),
+	testRegex: ".*\\.spec\\.ts$",
+	testEnvironment: "node",
+	coverageDirectory: "./cov"
 };
---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+import { defineConfig, configDefaults } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	test: {
+		globals: true,
+		environment: "node",
+		testNamePattern: ".*\\.spec\\.ts$",
+		exclude: [...configDefaults.exclude],
+		coverage: {
+			provider: "c8",
+			reporter: ["text", "html", "lcov"],
+			reportsDirectory: "coverage"
+		}
+	},
+	plugins: [tsconfigPaths()]
+});
