86620e61c65c4aeb36ad55cffa72daab06ea7f26
Dániel B. Papp <dpapp@odu.edu>
2023-01-20 14:23:30 -0500

    :wrench: (sentry): Added additional error tracking and tracing

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,13 @@ import * as Sentry from "@sentry/node";
 import sourceMapSupport from "source-map-support";
 
 async function bootstrap() {
-	const app = await NestFactory.create(
-		AppModule, 
-		{ 
-			cors: {
-				origin: "http://localhost:3000",
-				credentials: true
-			},
-			logger: ["error", "warn"]
-		}
-	);
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			origin: "http://localhost:3000",
+			credentials: true
+		},
+		logger: ["error", "warn", "debug", "verbose", "log"]
+	});
 
 	sourceMapSupport.install();
 
@@ -22,11 +19,21 @@ async function bootstrap() {
 	app.use(cookieParser());
 
 	await Sentry.init({
-		dsn:
-			"https://d7d1b5e63fd145218bf3971031bae0cd@o1009779.ingest.sentry.io/5974128",
+		dsn: "https://d7d1b5e63fd145218bf3971031bae0cd@o1009779.ingest.sentry.io/5974128",
 
-		tracesSampleRate: 1.0
+		tracesSampleRate: 1.0,
+		integrations: [new Sentry.Integrations.Http({ tracing: true })]
 	});
+
+	app.use(Sentry.Handlers.requestHandler());
+	app.use(
+		Sentry.Handlers.errorHandler({
+			shouldHandleError(error) {
+				// Capture all 404 and 500 errors
+				return error.status === 404 || error.status === 500;
+			}
+		})
+	);
 
 	await app.listen(process.env.PORT!, () =>
 		console.log(`🕵️‍ Listening on port ${process.env.PORT || 3000}...`)
