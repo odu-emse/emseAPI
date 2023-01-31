088a1269e2771bad70b9e446e7e87313e89fa467
Dániel B. Papp <dpapp@odu.edu>
2023-01-31 15:18:03 -0500

    :sparkles: (progress): Created CRUD resolvers and services

--- src/progress/progress.resolver.ts
+++ src/progress/progress.resolver.ts
@@ -1,7 +1,9 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { ProgressService } from "@/progress/progress.service";
 import { PlanOfStudyResolver } from "@/pos";
 import { ProgramResolver } from "@/program/program.resolver";
+import { ProgressArgs } from "@/types/graphql";
+import { Prisma } from "@prisma/client";
 
 @Resolver()
 export class ProgressResolver {
@@ -9,5 +11,44 @@ export class ProgressResolver {
 		private readonly progressService: ProgressService,
 		private readonly plan: PlanOfStudyResolver,
 		private readonly program: ProgramResolver
-	) {}
+	) {
+		this.progressService = progressService;
+		this.plan = plan;
+		this.program = program;
+	}
+	@Query("progress")
+	async progress(@Args("args") args: ProgressArgs) {
+		const response = await this.progressService.progress(args);
+		if (response instanceof Error) return new Error(response.message);
+		else return response;
+	}
+
+	@Mutation("createProgress")
+	async createProgress(
+		@Args("input") input: Prisma.ProgressCreateInput,
+		@Args("enrollmentID") enrollmentID: string
+	) {
+		const enrollment = await this.program.moduleEnrollment({
+			id: enrollmentID
+		});
+		console.log({ enrollmentID });
+		if (!enrollment || enrollment.length === 0)
+			return new Error("Enrollment not found");
+		const response = await this.progressService.createProgress(
+			input,
+			enrollmentID
+		);
+		if (response instanceof Error) return new Error(response.message);
+		else return response;
+	}
+
+	@Mutation("waiveModule")
+	async waiveModule(@Args("args") args: any) {
+		return await this.progressService.waiveModule(args);
+	}
+
+	@Mutation("deleteProgress")
+	async deleteProgress(@Args("id") id: string) {
+		return await this.progressService.deleteProgress(id);
+	}
 }
--- src/progress/progress.service.ts
+++ src/progress/progress.service.ts
@@ -1,7 +1,73 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "@/prisma.service";
+import { ProgressArgs } from "@/types/graphql";
+import { Prisma } from "@prisma/client";
 
 @Injectable()
 export class ProgressService {
 	constructor(private readonly prisma: PrismaService) {}
+	private progressIncludes = Prisma.validator<Prisma.ProgressInclude>()({
+		enrollment: true
+	});
+	async progress(args: ProgressArgs) {
+		const where = Prisma.validator<Prisma.ProgressWhereInput>()({
+			...(args.status && { status: args.status }),
+			...(args.completed && { completed: args.completed })
+		});
+
+		// if an id is provided, we can use it to find single progress since it is a unique field
+		if (args.id) {
+			const unique = Prisma.validator<Prisma.ProgressFindUniqueArgsBase>()({
+				where: { id: args.id },
+				include: this.progressIncludes
+			});
+			const res = this.prisma.progress.findUnique(unique);
+			if (!res) return new Error("Progress not found");
+			return [res];
+		}
+
+		// since enrollmentID is a unique field, we can use it to find a single progress
+		if (args.enrollmentID) {
+			const unique = Prisma.validator<Prisma.ProgressFindUniqueArgsBase>()({
+				where: { enrollmentID: args.enrollmentID },
+				include: this.progressIncludes
+			});
+			const res = this.prisma.progress.findUnique(unique);
+			if (!res) return new Error("Progress not found");
+			return [res];
+		}
+
+		const response = this.prisma.progress.findMany({
+			where,
+			include: this.progressIncludes
+		});
+		if (!response) return new Error("Progress not found");
+		return response;
+	}
+
+	async createProgress(
+		input: Prisma.ProgressCreateInput,
+		enrollmentID: string
+	) {
+		const args = Prisma.validator<Prisma.ProgressCreateArgs>()({
+			data: {
+				...input,
+				enrollment: {
+					connect: { id: enrollmentID }
+				}
+			},
+			include: this.progressIncludes
+		});
+		const res = this.prisma.progress.create(args);
+		if (!res) return new Error("Progress count not be created");
+		return res;
+	}
+
+	async deleteProgress(id: string) {
+		const res = this.prisma.progress.delete({ where: { id } });
+		if (!res) return new Error("Progress could not be deleted");
+		return true;
+	}
+
+	async waiveModule(args: any) {}
 }
