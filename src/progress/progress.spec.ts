268435d342568bc5dda6be22b4f47e4169bc0805
Dániel B. Papp <dpapp@odu.edu>
2023-02-01 15:01:24 -0500

    test(progress): Created mock data initialization and cleanup logic

--- src/progress/progress.spec.ts
+++ src/progress/progress.spec.ts
@@ -1,9 +1,20 @@
-import { ProgressService, ProgressResolver } from "@/progress";
+import { ProgressResolver, ProgressService } from "@/progress";
 import { PlanOfStudyResolver, PoSService } from "@/pos";
 import { ProgramResolver, ProgramService } from "@/program";
-import { Progress } from "@/types/graphql";
+import {
+	EnrollmentStatus,
+	PlanOfStudy,
+	Progress,
+	UserRole
+} from "../../gql/graphql";
 import { PrismaService } from "@/prisma.service";
-import { shuffle } from "../../utils/tests";
+import {
+	createEnrollment,
+	createModule,
+	createPlan,
+	shuffle
+} from "../../utils/tests";
+import { Prisma } from "@prisma/client";
 
 describe("Progress", function () {
 	let resolver: ProgressResolver;
@@ -14,20 +25,159 @@ describe("Progress", function () {
 	let planService: PoSService;
 	let dummyProgress: Progress;
 	let programService: ProgramService;
+	let dummyUserID = "63dabf67020a625cc55f64ff";
+
+	const planToDelete: Array<string> = [];
+	const moduleToDelete: Array<string> = [];
+	const enrollmentToDelete: Array<string> = [];
+	const progressToDelete: Array<string> = [];
+
+	let plan: PlanOfStudy;
+	let enrollment: Prisma.ModuleEnrollmentGetPayload<{
+		include: {
+			plan: {
+				include: {
+					student: true;
+				};
+			};
+			module: true;
+			progress: true;
+		};
+	}>;
+	let module: Prisma.ModuleGetPayload<{
+		include: {
+			members: {
+				include: {
+					plan: {
+						include: {
+							student: true;
+						};
+					};
+					progress: true;
+				};
+			};
+			assignments: {
+				include: {
+					assignmentResults: {
+						include: {
+							student: {
+								include: {
+									student: true;
+								};
+							};
+							gradedBy: {
+								include: {
+									social: true;
+									instructorProfile: true;
+								};
+							};
+						};
+					};
+				};
+			};
+			feedback: {
+				include: {
+					student: true;
+				};
+			};
+			parentModules: true;
+			subModules: true;
+			collections: {
+				include: {
+					lessons: {
+						include: {
+							threads: {
+								include: {
+									author: true;
+									comments: true;
+									usersWatching: true;
+									parentThread: true;
+								};
+							};
+							content: true;
+						};
+					};
+				};
+			};
+			course: true;
+		};
+	}>;
 
 	beforeAll(async () => {
 		prisma = new PrismaService();
-		progressService = new ProgressService(prisma);
+
+		// plan of study being instantiated here
 		planService = new PoSService(prisma);
 		planRes = new PlanOfStudyResolver(planService);
+
+		// program being instantiated here
 		programService = new ProgramService(prisma);
 		program = new ProgramResolver(programService);
+
+		// progress being instantiated here
+		progressService = new ProgressService(prisma);
 		resolver = new ProgressResolver(progressService, planRes, program);
+
+		const data = await initializeTest({
+			dummyUserID,
+			planRes,
+			program
+		});
+		plan = data.plan;
+		enrollment = data.enrollment;
+		module = data.module;
 	});
 	afterAll(async () => {
 		//delete created documents
+		//currently using native prisma delete, might need to be changed to delete services
+		for (const id of planToDelete) {
+			await prisma.planOfStudy.delete({ where: { id } });
+		}
+		for (const id of moduleToDelete) {
+			await prisma.module.delete({ where: { id } });
+		}
+		for (const id of enrollmentToDelete) {
+			await prisma.moduleEnrollment.delete({ where: { id } });
+		}
+		for (const id of progressToDelete) {
+			await prisma.progress.delete({ where: { id } });
+		}
 	});
-	describe("Create", function () {});
+	describe("Create", function () {
+		it("should create document", async function () {
+			const res = await resolver.createProgress(
+				{
+					enrollment: {
+						connect: {
+							id: enrollment.id
+						}
+					}
+				},
+				enrollment.id
+			);
+			if (res instanceof Error) throw new Error("Progress not created");
+			expect(res).toBeInstanceOf(Object);
+
+			//clean up
+			progressToDelete.push(res.id);
+			planToDelete.push(plan.id);
+			moduleToDelete.push(module.id);
+			enrollmentToDelete.push(enrollment.id);
+		});
+		it("should fail to create a document", async function () {
+			const res = await resolver.createProgress(
+				{
+					enrollment: {
+						connect: {
+							id: shuffle(enrollment.id)
+						}
+					}
+				},
+				shuffle(enrollment.id)
+			);
+			expect(res).toBeInstanceOf(Error);
+		});
+	});
 	describe("Read", function () {
 		it("should return an array of progress documents", async () => {
 			const result = await resolver.progress({});
@@ -46,3 +196,29 @@ describe("Progress", function () {
 	describe("Update", function () {});
 	describe("Delete", function () {});
 });
+
+const initializeTest = async ({ dummyUserID, planRes, program }) => {
+	const plan = await createPlan(planRes, { userID: dummyUserID });
+	if (plan instanceof Error) throw new Error("Plan not created");
+
+	const module = await createModule(program, {
+		moduleName: "Test",
+		moduleNumber: 1,
+		description: "This is a module created from a test case",
+		duration: 1,
+		intro: "This should not be seen by anyone",
+		keywords: ["test", "module"],
+		numSlides: 1
+	});
+	if (module instanceof Error) throw new Error("Module not created");
+
+	const enrollment = await createEnrollment(program, {
+		module: module.id,
+		plan: plan.id,
+		status: EnrollmentStatus.INACTIVE,
+		role: UserRole.STUDENT
+	});
+	if (enrollment instanceof Error) throw new Error("Enrollment not created");
+
+	return { plan, module, enrollment };
+};
--- utils/tests.ts
+++ utils/tests.ts
@@ -1,6 +1,52 @@
+import { PlanOfStudyResolver } from "@/pos";
+import { ProgramResolver } from "@/program";
+import { EnrollmentStatus, UserRole } from "@/types/graphql";
+
 export const shuffle = (str) =>
 	[...str].sort(() => Math.random() - 0.5).join("");
 
 export const pickRandomFromArray = (arr: any[]): number => {
 	return Math.floor(Math.random() * arr.length);
+};
+
+export const createPlan = async (
+	resolver: PlanOfStudyResolver,
+	config: { userID: string }
+) => {
+	const plan = await resolver.addPlan({
+		student: config.userID
+	});
+	if (plan) return plan;
+	else return new Error("Failed to create plan");
+};
+
+export const createModule = async (
+	resolver: ProgramResolver,
+	config: {
+		moduleName: string;
+		moduleNumber: number;
+		description: string;
+		duration: number;
+		intro: string;
+		keywords: string[];
+		numSlides: number;
+	}
+) => {
+	const module = await resolver.create({ ...config });
+	if (module) return module;
+	else return new Error("Failed to create module");
+};
+
+export const createEnrollment = async (
+	resolver: ProgramResolver,
+	config: {
+		module: string;
+		plan: string;
+		status: EnrollmentStatus;
+		role: UserRole;
+	}
+) => {
+	const enrollment = await resolver.addModuleEnrollment({ ...config });
+	if (enrollment) return enrollment;
+	else return new Error("Failed to create enrollment");
 };
