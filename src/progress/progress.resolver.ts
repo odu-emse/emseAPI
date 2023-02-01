fc19cf2ac8bbe6a238c5e0de7057c4b22fe0864f
Dániel B. Papp <dpapp@odu.edu>
2023-02-01 13:17:01 -0500

    bug(progress): Finished module waiver resolver logic

--- src/progress/progress.resolver.ts
+++ src/progress/progress.resolver.ts
@@ -2,7 +2,12 @@ import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { ProgressService } from "@/progress/progress.service";
 import { PlanOfStudyResolver } from "@/pos";
 import { ProgramResolver } from "@/program/program.resolver";
-import { ProgressArgs } from "@/types/graphql";
+import {
+	EnrollmentStatus,
+	ProgressArgs,
+	ProgressWaiveArgs,
+	UserRole
+} from "@/types/graphql";
 import { Prisma } from "@prisma/client";
 
 @Resolver()
@@ -31,7 +36,6 @@ export class ProgressResolver {
 		const enrollment = await this.program.moduleEnrollment({
 			id: enrollmentID
 		});
-		console.log({ enrollmentID });
 		if (!enrollment || enrollment.length === 0)
 			return new Error("Enrollment not found");
 		const response = await this.progressService.createProgress(
@@ -43,8 +47,109 @@ export class ProgressResolver {
 	}
 
 	@Mutation("waiveModule")
-	async waiveModule(@Args("args") args: any) {
-		return await this.progressService.waiveModule(args);
+	async waiveModule(@Args("args") args: ProgressWaiveArgs) {
+		// if moduleID and planID are provided, we can use them to find the enrollment or create a new one
+		if (args.moduleID && args.planID) {
+			const { moduleID, planID } = args;
+			// check if plan exists
+			const plan = await this.plan.planByParams({ id: planID });
+			if (plan instanceof Error) return new Error(plan.message);
+
+			// check if module exists
+			const module = await this.program.module(
+				{ id: moduleID },
+				UserRole.STUDENT
+			);
+			if (module instanceof Error) return new Error(module.message);
+			else {
+				// both module and plan exist, so we can create a new enrollment
+				const enrollment = await this.program.addModuleEnrollment({
+					module: moduleID,
+					plan: planID,
+					role: UserRole.STUDENT,
+					status: EnrollmentStatus.ACTIVE
+				});
+				// check if enrollment was created successfully
+				if (enrollment instanceof Error) return new Error(enrollment.message);
+				else {
+					// enrollment created, so we can create a new progress document
+					const progress = await this.createProgress(
+						{
+							status: 100,
+							completed: true,
+							enrollment: {
+								connect: {
+									id: enrollment.id
+								}
+							}
+						},
+						enrollment.id
+					);
+					if (progress instanceof Error) return new Error(progress.message);
+					// progress created, so we can return it
+					else return progress;
+				}
+			}
+		}
+		// if enrollmentID is provided, we can use it to find the enrollment and create a new progress document or find the existing one
+		else if (args.enrollmentID) {
+			const { enrollmentID } = args;
+			const enrollment = await this.program.moduleEnrollment({
+				id: enrollmentID
+			});
+			if (!enrollment) return new Error("Enrollment not found");
+
+			// since moduleEnrollment returns an array, we need to get the first element as it returns a unique enrollment if id is provided
+			const progress = enrollment[0].progress ? enrollment[0].progress : null;
+			// in case there is a population error we return an error
+			if (!enrollment[0].plan || !enrollment[0].module)
+				return new Error("Enrollment not found");
+			if (!progress) {
+				// progress doesn't exist, so we can create a new one
+				const res = await this.createProgress(
+					{
+						status: 100,
+						completed: true,
+						enrollment: {
+							connect: {
+								id: enrollmentID
+							}
+						}
+					},
+					enrollmentID
+				);
+				const enrollmentUpdate = await this.program.updateModuleEnrollment(
+					enrollmentID,
+					{
+						status: EnrollmentStatus.ACTIVE,
+						role: UserRole.STUDENT,
+						plan: enrollment[0].plan.id,
+						module: enrollment[0].module.id
+					}
+				);
+				if (res instanceof Error) return new Error(res.message);
+				if (enrollmentUpdate instanceof Error)
+					return new Error(enrollmentUpdate.message);
+				else return res;
+			} else if (progress) {
+				// progress exists, so we can update it
+				const res = await this.progressService.waiveModule(enrollmentID);
+				const enrollmentUpdate = await this.program.updateModuleEnrollment(
+					enrollmentID,
+					{
+						status: EnrollmentStatus.ACTIVE,
+						role: UserRole.STUDENT,
+						plan: enrollment[0].plan.id,
+						module: enrollment[0].module.id
+					}
+				);
+				if (res instanceof Error) return new Error(res.message);
+				if (enrollmentUpdate instanceof Error)
+					return new Error(enrollmentUpdate.message);
+				else return res;
+			}
+		} else
+			return new Error("No enrollment or module to be waived was specified");
 	}
 
 	@Mutation("deleteProgress")
