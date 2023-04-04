d91c4fce28bf24d5cd8b1ccea3537483142e3ca0
Dániel B. Papp <dpapp@odu.edu>
2023-04-04 12:58:05 -0400

    fix(community): Fixed schema issues that broke comment addition Because of mismatching argument names, comments got added to the first thread in the DB since it never got any appropriate parameters to search by.

--- src/community/community.resolver.ts
+++ src/community/community.resolver.ts
@@ -37,10 +37,13 @@ export class CommunityResolver {
 
 	@Mutation("addCommentToThread")
 	async addCommentToThread(
-		@Args("id") id: string,
+		@Args("parentThreadID") parentThreadID: string,
 		@Args("data") data: ICommentCreateInput
 	) {
-		const self = await this.thread({ id });
+		const self = await this.communityService.threadsByParam({
+			id: parentThreadID
+		});
+
 		if (self instanceof Error) return new Error(self.message);
 		else {
 			//creating new comment document
